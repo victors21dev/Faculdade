c5c6bca30966c719326fe5962eb2ce2397c2facd
Victor Santos <santos.victors2000@gmail.com>
2023-11-25 16:28:05 -0300

    Update MenuController.java

--- Experiência de Aprendizagem/Programação Orientada a Objetos/EA - 2/src/MenuController.java
+++ Experiência de Aprendizagem/Programação Orientada a Objetos/EA - 2/src/MenuController.java
@@ -39,9 +39,10 @@ public class MenuController {
                 System.out.println("---------------- Sessões disponíveis ----------------");
                 while(adicionando_ingresssos){
                     int opcao3;
-
+                    int escolha_filme = 1;
                     for(Ingresso Ingresso: sessoes){
-                        System.out.println(Ingresso.getId() + ") " + Ingresso.getHorario() + " em sala " + Ingresso.getSala() + ". Ingresso " + Ingresso.getTipo_ingresso() + " no unitário de R$" + Ingresso.getPreco() + ",00."  );
+                        System.out.println(escolha_filme + ") " + Ingresso.getHorario() + " em sala " + Ingresso.getSala() + ". Ingresso " + Ingresso.getTipo_ingresso() + " no unitário de R$" + Ingresso.getPreco() + ",00."  );
+                        escolha_filme++;
                     }
                     System.out.print("Digite o ingresso que deseja: "); opcao_ingresso = sc.nextInt();
                     System.out.print("1) Inteira. 2) Meia: "); tipo = sc.nextInt();
@@ -57,8 +58,10 @@ public class MenuController {
                         System.out.println("Opção invalída em quantidade de ingressos.");
                         adicionando_ingresssos = false;
                     }else{
+
 //                      Salvar lista ingressos
-                        compras.add(new Compra_Impressos(qtd_array,opcao_ingresso, tipo, quantidade_ingresso));
+                        salvar_Ingressos(compras, qtd_array, opcao_ingresso, tipo, quantidade_ingresso);
+
                         qtd_array++;
 
                         System.out.print("Deseja comprar mais ingressos? 1) Sim. 2) Não: "); opcao3 = sc.nextInt();
@@ -68,38 +71,7 @@ public class MenuController {
                                 System.out.println();
                                 escolha_if = false;
                             } else if (opcao3 == 2) {
-                                System.out.println("Legal! Aqui a sua notinha abaixo:");
-                                System.out.println();
-                                System.out.println("---------------- Nota - CinemaMax ----------------");
-                                System.out.println();
-                                System.out.println("____Sobre o filme____");
-                                System.out.print("Filme " + filmes.get(opcao - 1).getNome() + " de " + filmes.get(opcao - 1).getAno() + ". Com " + filmes.get(opcao - 1).getDuracao() + " de duração.");
-                                System.out.println(filmes.get(opcao - 1).getDescricao());
-                                System.out.println("Diretor: " + filmes.get(opcao - 1).getDiretor() + ". Gênero: " + filmes.get(opcao - 1).getGenero());
-                                System.out.println();
-                                System.out.println("____Ingressos____");
-                                int qtd = 1;
-                                int total_pagar = 0;
-                                for(Compra_Impressos Compra: compras){
-                                    int preco_ingresso;
-                                    String tipo_in;
-                                    if(Compra.getTipo() == 2){
-                                        preco_ingresso = (sessoes.get(Compra.getOpcao()-1).getPreco() / 2);
-                                        total_pagar = (total_pagar + sessoes.get(Compra.getOpcao()-1).getPreco() / 2);
-                                        tipo_in = "Meia";
-                                    }else{
-                                        preco_ingresso = sessoes.get(Compra.getOpcao()-1).getPreco();
-                                        tipo_in = "Inteira";
-                                        total_pagar = (total_pagar + sessoes.get(Compra.getOpcao()-1).getPreco());
-                                    }
-                                    System.out.println(qtd + " - R$" + preco_ingresso + ",00. Quantidade: " + Compra.getQtd() + " ingressso: " + tipo_in);
-
-                                    qtd++;
-                                }
-                                System.out.println();
-                                System.out.println("____Total____");
-                                System.out.print("Total a pagar R$ - " + total_pagar + ",00.");
-
+                                imprimir_nota(filmes, sessoes, compras, opcao);
                                 escolha_if = false;
                                 adicionando_ingresssos = false;
                                 repeticao = false;
@@ -109,8 +81,45 @@ public class MenuController {
                         }
                     }
                 }
-
             }
         }
+    }
+//  Salvamento na lista compras
+    private void salvar_Ingressos(List<Compra_Impressos> compras, int qtd_array,int opcao_ingresso, int tipo, int quantidade_ingresso){
+        compras.add(new Compra_Impressos(qtd_array,opcao_ingresso, tipo, quantidade_ingresso));
+    }
+//  Mostrar na tela a nota
+    private void imprimir_nota(List<Filme> filmes, List<Ingresso> sessoes, List<Compra_Impressos> compras, int opcao){
+        System.out.println("Legal! Aqui a sua notinha abaixo:");
+        System.out.println();
+        System.out.println("---------------- Nota - CinemaMax ----------------");
+        System.out.println();
+        System.out.println("____Sobre o filme____");
+        System.out.print("Filme " + filmes.get(opcao - 1).getNome() + " de " + filmes.get(opcao - 1).getAno() + ". Com " + filmes.get(opcao - 1).getDuracao() + " de duração.");
+        System.out.println(filmes.get(opcao - 1).getDescricao());
+        System.out.println("Diretor: " + filmes.get(opcao - 1).getDiretor() + ". Gênero: " + filmes.get(opcao - 1).getGenero());
+        System.out.println();
+        System.out.println("____Ingressos____");
+        int qtd = 1;
+        int total_pagar = 0;
+        for(Compra_Impressos Compra: compras){
+            int preco_ingresso;
+            String tipo_in;
+            if(Compra.getTipo() == 2){
+                preco_ingresso = (sessoes.get(Compra.getOpcao()-1).getPreco() / 2);
+                total_pagar = (total_pagar + sessoes.get(Compra.getOpcao()-1).getPreco() / 2);
+                tipo_in = "Meia";
+            }else{
+                preco_ingresso = sessoes.get(Compra.getOpcao()-1).getPreco();
+                tipo_in = "Inteira";
+                total_pagar = (total_pagar + sessoes.get(Compra.getOpcao()-1).getPreco());
+            }
+            System.out.println(qtd + " - R$" + preco_ingresso + ",00. Quantidade: " + Compra.getQtd() + " ingressso: " + tipo_in);
+
+            qtd++;
+        }
+        System.out.println();
+        System.out.println("____Total____");
+        System.out.print("Total a pagar R$ - " + total_pagar + ",00.");
     }
 }
